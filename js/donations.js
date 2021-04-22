87c2d0a8d0d1de46d59ac6997aa9acfa8518461e
AbdullahElian1 <abodelian28@gmail.com>
2021-04-22 21:01:55 +0300

    add local storage to the donation page

--- js/donations.js
+++ js/donations.js
@@ -5,6 +5,7 @@ function Donation(CategoryType, source, contInfo,desc){
     this.source=source;
     this.contInfo=contInfo;
     this.desc=desc;
+    this.value=0;
 
     Donation.allItems.push(this);
 
@@ -15,6 +16,30 @@ Donation.allItems=[];
 
 // let x= new Donation('food','hjk','dssd','dfj');
 // console.log(x);
+
+
+function saveToLs(){
+    
+    let arrStr = JSON.stringify( Donation.allItems);
+    localStorage.setItem('Itemsaved', arrStr);
+    
+  }
+
+  function gettingOrderFromLs(){
+    
+    let data = localStorage.getItem('Itemsaved');
+    //console.log(data);
+    let order = JSON.parse(data);
+    //console.log(order);
+
+    if(order !== null){
+      Donation.allItems = order;
+    }
+    console.log(Donation.allItems);
+     
+    renderls();
+  }
+  
 
 
 let formItem= document.getElementById('form1');
@@ -37,6 +62,7 @@ function handleSubmit(event){
      new Donation(catg,img,contus,description);
 
      console.log(Donation.allItems);
+     saveToLs();
     render();
 
 }
@@ -64,9 +90,47 @@ function render(){
     Di.appendChild(contact);
     contact.textContent=contus;
 
+    let but=document.createElement('button');
+    Di.appendChild(but);
+    but.textContent="Book It!";
+
 
 
 }
+
+function renderls(){
+    
+    for(let i=0;i<Donation.allItems.length;i++){
+        let Di=document.createElement('div');
+        Di.setAttribute('class','Items');
+       secTwo.appendChild(Di);
+
+       let img1=document.createElement('img');
+    img1.setAttribute("src",`${Donation.allItems[i].source}`);
+    Di.appendChild(img1);
+
+    let catg1=document.createElement('h4');
+    Di.appendChild(catg1);
+    catg1.textContent=`${Donation.allItems[i].type}`;
+
+    let desc=document.createElement('p');
+    Di.appendChild(desc);
+    desc.textContent=`${Donation.allItems[i].desc}`;
+
+    
+    let contact=document.createElement('p');
+    Di.appendChild(contact);
+    contact.textContent=`${Donation.allItems[i].contInfo}`;
+
+    let but=document.createElement('button');
+    Di.appendChild(but);
+    but.textContent="Book It!";
+
+     }
+}
+
+
+gettingOrderFromLs();
 
 
 
