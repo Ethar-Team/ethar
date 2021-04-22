9f6f31b424988879d058b46f9f51e24922fce955
anas <civil.anas1000@gmail.com>
2021-04-22 17:49:21 +0300

    adding function

--- js/services.js
+++ js/services.js
@@ -1,12 +1,14 @@
 'use sttict '
 
+let arryOfHelp=[];  
 function AskForHelp (names,countatNumber,helpTime){
 
     this.name=names;
     this.countatNumber=countatNumber;
     this.helpTime=helpTime;
-
+    arryOfHelp.push(this)
 }
 
 let anas = new AskForHelp('anas','11','12')
 
+console.log(anas)
