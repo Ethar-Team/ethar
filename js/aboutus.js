1b100953fb63a423b70a6bd9f5d4ed51ed949d17
RoaaMustafa <labushanab14@gmail.com>
2021-04-22 16:47:46 +0300

    adding aboutus.js

---
+++ js/aboutus.js
@@ -0,0 +1,17 @@
+'use script';
+let container =document.getElementById('sectwo');
+let linkes =document.getElementById('secthree');
+function About (name,breif,linkedin,github){
+  this.name=name;
+  this.breif=breif;
+  this.linkedinAcc=linkedin;
+  this.githubAcc=github;
+}
+//adding the instances
+new About('Ayyoub Alkeyyam','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','http://www.linkedin.com/in/ayyoubk','https://github.com/ayyoubk');
+new About('Mohammad Hayagneh ','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','https://www.linkedin.com/in/mohammad-hayajneh-2ab099124/','https://github.com/mohammadhayajneh2014');
+new About ('Shatha Alsmadi','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','https://www.linkedin.com/in/shatha-al-smadi-5348021ab/','https://github.com/shathasmadi');
+new About ('Roaa Mustafa','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','https://www.linkedin.com/in/roaa-mustafa98/','https://github.com/RoaaMustafa');
+new About ('Abdullah Elian','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','http://www.linkedin.com/in/abdullah-elian','https://github.com/AbdullahElian1');
+new About ('Anas Abu-Galiha','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','https://www.linkedin.com/in/anas-abughalieh-68b73a194/','https://github.com/AnasAGc');
+new About('Aysheh Abu mahfouz','Lorem ipsum dolor sit amet consectetur adipisicing elit. Error doloribus dolor facilis expedita corporis dignissimos similique assumenda odit, reprehenderit qui neque necessitatibus praesentium quos, dolorum vel? Quidem exercitationem quam tempore!','https://www.linkedin.com/in/aysheh-mahfouz-28a48a20b/','https://github.com/Aysheh-Mahfouz');
