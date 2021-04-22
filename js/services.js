8b1f707c06d4013d29091d5a20fd22e99f258099
anas <civil.anas1000@gmail.com>
2021-04-22 18:57:22 +0300

    adding function listener

--- js/services.js
+++ js/services.js
@@ -1,14 +1,49 @@
 'use sttict '
 
-let arryOfHelp=[];  
+let helpingArray=[];  
+let waitinglist=document.getElementById("waitingList")
+
 function AskForHelp (names,countatNumber,helpTime){
 
-    this.name=names;
+    this.names=names;
     this.countatNumber=countatNumber;
     this.helpTime=helpTime;
-    arryOfHelp.push(this)
+    helpingArray.push(this)
 }
 
 let anas = new AskForHelp('anas','11','12')
 
-console.log(anas)
+console.log(helpingArray.names)
+
+
+const helpingForm=document.getElementById("hepingForm")
+helpingForm.addEventListener('submit', helpTask)
+
+
+
+ function helpTask (event){
+
+    event.preventDefault()
+
+    let newName=event.target.customername.value
+    let newContact=event.target.customercontact.value
+    let newTime=event.target.time.value
+
+    let Newhelp= new AskForHelp(newName,newContact,newTime)
+
+}
+
+
+
+
+
+let list=document.createElement("ul")
+waitinglist.appendChild(list)
+randerlist ()
+
+function randerlist (){
+
+    let li=document.createElement('li')
+    list.appendChild(li)
+    // li.textContent=('THe Name : ' + helpingArray.names[0] + ' The Contact Number ' )
+}
