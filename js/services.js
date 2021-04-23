21116acf55be0b52fddf1b10e9595dedcca9d187
anas <civil.anas1000@gmail.com>
2021-04-23 12:17:50 +0300

    adding form from js

--- js/services.js
+++ js/services.js
@@ -32,16 +32,145 @@ console.log(array[0].names)
 
 
 
-const cards=document.getElementById("right")
-const container=document.getElementById("left")
+const cards=document.getElementById("right");
+const container=document.getElementById("left");
+let formSection=document.createElement("section");
+
+
 cards.addEventListener('click',renderform)
  
     function renderform (event){
+
+        formSection.remove();
+
+        formSection=document.createElement("section");
+        formSection.setAttribute("id","left-sec")
+        container.appendChild(formSection);
+
         let title=document.createElement("h2")
-        container.appendChild(title)
-        console.log(event.target.id)
+        formSection.appendChild(title)
+        let category =event.target.id
+        title.textContent=category.toUpperCase()
+
+        if (category==="maintenance"){
+
+            let paragraph1=document.createElement("p")
+            formSection.appendChild(paragraph1)
+            paragraph1.textContent="Paragraph1"
+
+            let maintenanceForm=document.createElement("form")
+            formSection.appendChild(maintenanceForm)
+            let fieldset=document.createElement("fieldset")
+            maintenanceForm.appendChild(fieldset)
+
+            let legend=document.createElement("legend")
+            fieldset.appendChild(legend)
+            legend.textContent="Ask For Help"
+
+            let nameLabel=document.createElement("label")
+            fieldset.appendChild(nameLabel)
+            nameLabel.textContent="Name"
+            nameLabel.setAttribute("for","customername")
+            let input1=document.createElement("input")
+            fieldset.appendChild(input1)
+            input1.setAttribute("type","text")
+            input1.setAttribute("id","customername")
+            let br1=document.createElement("br")
+
+
+            let nameLabe2=document.createElement("label")
+            fieldset.appendChild(nameLabe2)
+            nameLabe2.textContent="Contact"
+            nameLabe2.setAttribute("for","customercontact")
+            let input2=document.createElement("input")
+            fieldset.appendChild(input2)
+            input2.setAttribute("type","text")
+            input2.setAttribute("id","customercontact")
+            let br2=document.createElement("br")
+
+            
+            let nameLabe3=document.createElement("label")
+            fieldset.appendChild(nameLabe3)
+            nameLabe3.textContent="Description"
+            nameLabe3.setAttribute("for","description")
+            let input3=document.createElement("input")
+            fieldset.appendChild(input3)
+            input3.setAttribute("type","text")
+            input3.setAttribute("id","description")
+            let br3=document.createElement("br")
+
+            let submit=document.createElement("input")
+            fieldset.appendChild(submit)
+            submit.setAttribute("type","submit")
+            submit.setAttribute("value", "Submit")
+        }
+
+
+
+        if (category==="teaching"){
+
+            let paragraph1=document.createElement("p")
+            formSection.appendChild(paragraph1)
+            paragraph1.textContent="Paragraph2"
+
+            let teachingForm=document.createElement("form")
+            teachingForm.appendChild(teachingForm)
+            let fieldset=document.createElement("fieldset")
+            teachingForm.appendChild(fieldset)
+
+            let legend=document.createElement("legend")
+            fieldset.appendChild(legend)
+            legend.textContent="Ask For Help"
+
+            let nameLabel=document.createElement("label")
+            fieldset.appendChild(nameLabel)
+            nameLabel.textContent="Name"
+            nameLabel.setAttribute("for","customername")
+            let input1=document.createElement("input")
+            fieldset.appendChild(input1)
+            input1.setAttribute("type","text")
+            input1.setAttribute("id","customername")
+            let br1=document.createElement("br")
+
+
+            let nameLabe2=document.createElement("label")
+            fieldset.appendChild(nameLabe2)
+            nameLabe2.textContent="Contact"
+            nameLabe2.setAttribute("for","customercontact")
+            let input2=document.createElement("input")
+            fieldset.appendChild(input2)
+            input2.setAttribute("type","text")
+            input2.setAttribute("id","customercontact")
+            let br2=document.createElement("br")
+
+            
+            let nameLabe3=document.createElement("label")
+            fieldset.appendChild(nameLabe3)
+            nameLabe3.textContent="Description"
+            nameLabe3.setAttribute("for","description")
+            let input3=document.createElement("input")
+            fieldset.appendChild(input3)
+            input3.setAttribute("type","text")
+            input3.setAttribute("id","description")
+            let br3=document.createElement("br")
+
+            let submit=document.createElement("input")
+            fieldset.appendChild(submit)
+            submit.setAttribute("type","submit")
+            submit.setAttribute("value", "Submit")
+        }
+
+
+
+
     }
 
+
+
+    function randerSection (){
+
+        
+    }
 
 let list=document.createElement("ul")
 waitinglist.appendChild(list)
